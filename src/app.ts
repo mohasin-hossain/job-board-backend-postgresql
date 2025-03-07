646924c44652e7f320f2e9c664e73cb2187c75a1
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-08 00:49:31 +0100

    Routes pattern

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './routes';
 
 const app: Application = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Application Routes
+app.use('/api/v1', router);
 
 const getController = (req: Request, res: Response) => {
   res.status(200).json({
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.use('/jobs');
+router.use('/applications');
+
+export default router;
