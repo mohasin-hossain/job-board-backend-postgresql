63746533a415fc2dc5b2c46241cf6e608f977879
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-08 03:10:42 +0100

    [REFACTOR]: Job Service

--- src/modules/jobs/jobs.service.ts
+++ src/modules/jobs/jobs.service.ts
@@ -1,19 +1,16 @@
 import { IJob } from './jobs.interface';
 import { JobModel } from './jobs.model';
 
-const createJobIntoDB = async (jobData: IJob) => {
-  const result = await JobModel.create(jobData);
-  return result;
+const createJobIntoDB = async (jobData: IJob): Promise<IJob> => {
+  return await JobModel.create(jobData);
 };
 
-const getAllJobsFromDB = async () => {
-  const result = await JobModel.getAll();
-  return result;
+const getAllJobsFromDB = async (): Promise<IJob[]> => {
+  return await JobModel.getAll();
 };
 
-const getJobByIdFromDB = async (id: number) => {
-  const result = await JobModel.getById(id);
-  return result;
+const getJobByIdFromDB = async (id: number): Promise<IJob | null> => {
+  return await JobModel.getById(id);
 };
 
 export const JobService = {
