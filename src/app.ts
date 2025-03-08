1ed43509d7aff4f209ee3077ae78226b9d386387
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-08 12:59:36 +0100

    Added app folder

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import router from './routes';
-import globalErrorHandler from './middlewares/globalErrorHandler';
-import notFound from './middlewares/notFound';
+import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
