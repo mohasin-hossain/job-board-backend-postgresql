15edf0a0d267096bb5a54ea94f3bc3637a5feada
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-08 03:00:12 +0100

    [REFACTOR]: Added sendResponse util in all jobs API

--- src/modules/jobs/jobs.controller.ts
+++ src/modules/jobs/jobs.controller.ts
@@ -1,33 +1,37 @@
 import { Request, Response } from 'express';
 import { JobService } from './jobs.service';
+import sendResponse from '../../utils/sendResponse';
 
 const createJob = async (req: Request, res: Response) => {
-  const result = await JobService.createJobIntoDB(req.body);
+  const job = await JobService.createJobIntoDB(req.body);
 
-  res.status(200).json({
+  sendResponse(res, {
     success: true,
-    message: 'Job Created Successfully!',
-    data: result,
+    statusCode: 201,
+    message: 'Job created successfully',
+    data: job,
   });
 };
 
 const getAllJobs = async (req: Request, res: Response) => {
-  const result = await JobService.getAllJobsFromDB();
+  const jobs = await JobService.getAllJobsFromDB();
 
-  res.status(200).json({
+  sendResponse(res, {
     success: true,
+    statusCode: 200,
     message: 'Jobs retrieved successfully!',
-    data: result,
+    data: jobs,
   });
 };
 
 const getJobById = async (req: Request, res: Response) => {
-  const result = await JobService.getJobByIdFromDB(Number(req.params.id));
+  const job = await JobService.getJobByIdFromDB(Number(req.params.id));
 
-  res.status(200).json({
-    success: true,
-    message: 'Jobs retrieved successfully!',
-    data: result,
+  sendResponse(res, {
+    success: !!job,
+    statusCode: job ? 200 : 404,
+    message: job ? 'Job found!' : 'Job not found!',
+    data: job,
   });
 };
 
